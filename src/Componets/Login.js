dd3475150e7b000fdcb32b999a9f8446b319846f
unknown <FrosinaG>
2023-03-05 11:22:15 +0100

    merge

--- src/Componets/Login.js
+++ src/Componets/Login.js
@@ -21,6 +21,7 @@ const Login = () => {
   const login = e => {
     e.preventDefault();
     dispatch({type: LOGIN, username, password, navigate});
+
   };
 
   const user = (e) => {
--- src/sagas/loginSaga.js
+++ src/sagas/loginSaga.js
@@ -1,38 +1,35 @@
 import axios from "axios";
 import { call, put, takeEvery } from "redux-saga/effects";
-import {
-    SET_LOGIN,
-    LOGIN
-} from "../actions/actionTypes";
+import { SET_LOGIN, LOGIN } from "../actions/actionTypes";
 
 function userLogin(username, password) {
-    return axios
-      .post("http://localhost:5000/users/login", null, {
-        params: {
-          username,
-          password,
-        },
-      })
-      .then(response => response)
-      .catch(error => {
-          console.log(error);
-          return error
-      });
+  return axios
+    .post("http://localhost:5000/users/login", null, {
+      params: {
+        username,
+        password,
+      },
+    })
+    .then((response) => response)
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
 }
 
 function* login(payload) {
-    const username = payload.username || ""
-    const password = payload.password || ""
-    const navigate = payload.navigate || null
-    const response = yield call(userLogin, username, password);
-    if (response.status === 200) {
-        yield put({ type: SET_LOGIN, user: response?.data || {} })
-        navigate("/shop")
-    }
+  const username = payload.username || "";
+  const password = payload.password || "";
+  const navigate = payload.navigate || null;
+  const response = yield call(userLogin, username, password);
+  if (response.status === 200) {
+    yield put({ type: SET_LOGIN, user: response?.data || {} });
+    navigate("/shop");
+  }
 }
 
 function* loginSaga() {
-    yield takeEvery(LOGIN, login);
+  yield takeEvery(LOGIN, login);
 }
 
 export default loginSaga;
